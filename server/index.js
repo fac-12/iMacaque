1343164cef874e24179df3521c34804632bf9621
Natalie <njseeto@users.noreply.github.com>
2018-03-07 18:09:10 +0000

    add production code

--- server/index.js
+++ server/index.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 require("./models/Binary_data");
 require("./models/Static_data");
 mongoose.connect(process.env.DATABASE_URL);
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve("/app/client/build/index.html"));
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  })
 }
+
 const port = process.env.PORT || 8000;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log("App is listening on " + port);
 });
