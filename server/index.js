37387062ffd9417fc4685a66e1df39ce9f1f6023
Gbaja <gbajaf@yahoo.co.uk>
2018-03-07 17:49:07 +0000

    added production build

--- server/index.js
+++ server/index.js
@@ -6,6 +6,12 @@ mongoose.connect(process.env.DATABASE_URL);
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve("/app/client/build/index.html"));
+  });
+}
 const port = process.env.PORT || 8000;
 
 app.listen(port, function() {
