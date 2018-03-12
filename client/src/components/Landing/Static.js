5d2d41ee82154ae0a8c0fd0b647b76020850f170
Natalie <njseeto@users.noreply.github.com>
2018-03-12 10:54:22 +0000

    set up landing component

---
+++ client/src/components/Landing/Static.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Static extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Static Test </h1>
+      </div>
+    );
+  }
+}
+
+export default Static;
