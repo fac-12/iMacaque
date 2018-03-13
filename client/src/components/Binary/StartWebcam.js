1845e5f6ffeaab94d7ec94fad7c157a42b161522
Natalie <njseeto@users.noreply.github.com>
2018-03-13 12:25:25 +0000

    working camera function relates to #1

--- client/src/components/Binary/StartWebcam.js
+++ client/src/components/Binary/StartWebcam.js
@@ -2,8 +2,27 @@ import React, { Component } from "react";
 import Webcam from "react-webcam";
 
 class StartWebcam extends Component {
+  setRef = webcam => {
+    this.webcam = webcam;
+  };
+
+  capture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+    console.log("hello: ", imageSrc);
+  };
+
   render() {
-    return <Webcam />;
+    return (
+      <div onClick={this.capture}>
+        <Webcam
+          audio={false}
+          height={700}
+          ref={node => (this.webcam = node)}
+          screenshotFormat="image/jpeg"
+          width={1000}
+        />
+      </div>
+    );
   }
 }
 
