64cc521a55a921016e37cf639377ae505a61103b
Natalie <njseeto@users.noreply.github.com>
2018-03-15 15:57:17 +0000

    add static data to database relates to #10

--- server/models/Static_data.js
+++ server/models/Static_data.js
@@ -3,17 +3,8 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const staticDataSchema = new Schema({
-  trialId: Number,
   monkeyId: String,
-  A: Boolean,
-  B: Boolean,
-  C: Boolean,
-  D: Boolean,
-  E: Boolean,
-  F: Boolean,
-  G: Boolean,
-  H: Boolean,
-  I: Boolean,
+  selected: String,
   timestamp: Date
 });
 
