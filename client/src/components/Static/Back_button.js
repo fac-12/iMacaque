4c1266b74b0dc141fe3ed08b5489dae3f5445f8e
Gbaja <gbajaf@yahoo.co.uk>
2018-03-20 15:19:15 +0000

    static css relates to #31

--- client/src/components/Static/Back_button.js
+++ client/src/components/Static/Back_button.js
@@ -1,26 +1,26 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-
+import "./Static.css";
 class StaticBackBtn extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0
+      count: 1
     };
   }
 
   handleClick = () => {
     this.setState({ count: this.state.count + 1 });
     console.log("countttt: ", this.state.count);
-    if (this.state.count === 5) {
+    if (this.state.count === 8) {
       console.log("clickedddd");
       window.location = "/";
     }
   };
   render() {
     return (
-      <div className="static-fractals__back-btn" onClick={this.handleClick}>
-        <button>Home</button>
+      <div onClick={this.handleClick}>
+        <button className="static-fractals__back-btn">Home</button>
       </div>
     );
   }
